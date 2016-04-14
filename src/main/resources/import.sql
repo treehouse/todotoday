9940e10378f0f2918573250bd79bdfb4a951623e
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-04-14 10:11:17 -0500

    Added User & Role entities to application

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+-- Insert role
+insert into role (name) values ('ROLE_USER')
+
+-- Insert two users
+insert into user (username,enabled,password) values ('user',true,'password',1)
+insert into user (username,enabled,password) values ('user2',true,'password',1)
+
 -- Insert tasks
 insert into task (complete,description) values (true,'Code Task entity');
 insert into task (complete,description) values (false,'Discuss users and roles');
